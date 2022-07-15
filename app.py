574f512ef72de6446d2b4f11714021d276af9978
lustered <smellslikelavenders@gmail.com>
2022-07-14 22:56:44 -0400

    Heroku troubleshoot

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 import os
-
-# init SQLAlchemy so we can use it later in our models
-db = SQLAlchemy()
+from models import db
 
 def create_app():
     app = Flask(__name__)
@@ -18,7 +16,7 @@ def create_app():
     login_manager.login_view = "auth.login"
     login_manager.init_app(app)
 
-    from .models import User
+    from models import User
 
     @login_manager.user_loader
     def load_user(user_id):
@@ -26,23 +24,23 @@ def create_app():
         return User.query.get(int(user_id))
 
     # blueprint for auth routes in our app
-    from .auth import auth as auth_blueprint
+    from auth import auth as auth_blueprint
 
     app.register_blueprint(auth_blueprint)
 
     # blueprint for non-auth parts of app
-    from .main import main as main_blueprint
+    from main import main as main_blueprint
 
     app.register_blueprint(main_blueprint)
 
     return app
 
 
-app = create_app()
 
-# if __name__ == "__main__":
-#     app.run(
-#         host=os.getenv("IP", "0.0.0.0"),
-#         port=int(os.getenv("PORT", 5050)),
-#         debug=True,
-#     )
+app = create_app()
+if __name__ == "__main__":
+    app.run(
+        host=os.getenv("IP", "0.0.0.0"),
+        port=int(os.getenv("PORT", 5050)),
+        debug=True,
+    )
